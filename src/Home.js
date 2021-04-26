cb39cbe73010ec3a09859639d5b0c11574e3778a
dorischen <doris@rushhourmedia.hk>
2021-04-26 10:06:04 +0800

    Full React Tutorial #1-8

---
+++ src/Home.js
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+
+const Home = () => {
+    // let name = 'mario';
+    const [name, setName] = useState('mario');
+    const [age, setAge] = useState(25);
+
+    const handleClick = (e) => {
+        setName('luigi');
+        setAge(30);
+    }
+
+    return (  
+        <div className="Home">
+            <h2>Homepage</h2>
+            <p>{ name } is { age } years old</p>
+            <button onClick={handleClick}>Click me</button>
+        </div>
+    );
+} 
+ 
+export default Home;
