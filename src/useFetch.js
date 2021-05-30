afc42c07689424df134ec75716415bd20f2026fa
dorischen <doris@rushhourdigital.com>
2021-05-30 20:59:45 +0800

    Full React Tutorial #21-32

--- src/useFetch.js
+++ src/useFetch.js
@@ -6,8 +6,10 @@ const useFetch = (url) => {
     const [error, setError] = useState(null);
 
     useEffect(() => {
+        const abortCont = new AbortController();
+
         setTimeout(() => {
-            fetch(url)
+            fetch(url, { signal: abortCont.signal })
                 .then(res => {
                     if (!res.ok) {
                         throw Error('could not fetch the data for that resource');
@@ -20,10 +22,16 @@ const useFetch = (url) => {
                     setError(null);
                 })
                 .catch(err => {
-                    setIsPending(false);
-                    setError(err.message);
+                    if (err.name === 'AbortError') {
+                        console.log('fetch aborted');
+                    } else {
+                        setIsPending(false);
+                        setError(err.message);
+                    }
                 })
         }, 1000);
+
+        return () => abortCont.abort();
     }, [url]);
 
     return { data, isPending, error };
