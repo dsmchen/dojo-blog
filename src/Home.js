c9f8e6ba2d30c7e08d6d3e1c9bd2158914976764
dorischen <doris@rushhourdigital.com>
2021-05-30 19:17:53 +0800

    Full React Tutorial #18-20

--- src/Home.js
+++ src/Home.js
@@ -1,21 +1,14 @@
 import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
+import useFetch from './useFetch';
 
 const Home = () => {
-    const [blogs, setBlogs] = useState(null);
-
-    useEffect(() => {
-        fetch('http://localhost:8000/blogs')
-            .then(res => {
-                return res.json();
-            })
-            .then((data) => {
-                setBlogs(data);
-            })
-    }, []);
+    const { data: blogs, isPending, error } = useFetch('http://localhost:8000/blogs');
 
     return (  
         <div className="home">
+            {error && <div>{error}</div>}
+            {isPending && <div>Loading...</div>}
             {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
         </div>
     );
